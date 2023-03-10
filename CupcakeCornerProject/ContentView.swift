e0de945c939739b95f1c16de6c7f3ec88851ffdd
Emma Gutierrez <eclaire.tv@gmail.com>
2023-03-10 01:51:01 -0800

    Initial Commit

---
+++ CupcakeCornerProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CupcakeCornerProject
+//
+//  Created by Emma Gutierrez on 3/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupcakeCornerProject/CupcakeCornerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerProjectApp.swift
+//  CupcakeCornerProject
+//
+//  Created by Emma Gutierrez on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
